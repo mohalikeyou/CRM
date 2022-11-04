55c113e77d39b4f3d83ed89594a608053dfb1f23
Pokaman <mm1314955811@163.com>
2022-11-04 21:39:57 +0800

    01 首页功能

---
+++ crm2022/src/main/java/com/bjpowernode/crm/settings/web/controller/UserLogin.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.crm.settings.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserLogin {
+
+    @RequestMapping("/settings/qx/user/toLogin.do")
+    public String toLogin() {
+        return "settings/qx/user/login";
+    }
+}
