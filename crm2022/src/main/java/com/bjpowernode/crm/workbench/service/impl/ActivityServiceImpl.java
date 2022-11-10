9b2d3f4c1c857c656bf01f5bc2808d0a4526b312
Pokaman <mm1314955811@163.com>
2022-11-10 15:13:02 +0800

    08 查询市场活动（需求1，需求2)

--- crm2022/src/main/java/com/bjpowernode/crm/workbench/service/impl/ActivityServiceImpl.java
+++ crm2022/src/main/java/com/bjpowernode/crm/workbench/service/impl/ActivityServiceImpl.java
@@ -7,6 +7,9 @@ import com.bjpowernode.crm.workbench.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class ActivityServiceImpl implements ActivityService {
 
@@ -15,5 +18,15 @@ public class ActivityServiceImpl implements ActivityService {
     @Override
     public int saveActivity(Activity activity) {
         return activityMapper.insertActivity(activity);
+    }
+
+    @Override
+    public List<Activity> queryActivitiesByConditionsForPage(Map<String, Object> map) {
+        return activityMapper.selectActivitiesByConditionsForPage(map);
+    }
+
+    @Override
+    public int queryCountOfActivitiesByConditions(Map<String, Object> map) {
+        return activityMapper.selectCountOfActivitiesByConditions(map);
     }
 }
